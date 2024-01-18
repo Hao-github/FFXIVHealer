5921717bf76119d96bce864c2763f89c2cb828fb
RobinL <haobin0228@gmail.com>
2024-01-19 00:27:15 +0800

    打包完咯

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
 from Fight import Fight
 
 
 if __name__ == "__main__":
-    Fight.addbaseCofig("P9S.xlsx")
-    Fight.run(0.01)
+    try:
+        Fight.addbaseCofig("P9S.xlsx")
+        Fight.run(0.01)
+        print("结果已输出至output.csv")
+    except Exception as e:
+        print(e)
+        print("报错惹, 把报错的原因和结果都截个图记录一下一起发给我")
+    finally:
+        os.system("pause")
